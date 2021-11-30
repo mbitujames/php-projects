275640758f1b2936161313e9c70b2762669facd7
mbitu <mbitumutonga@gmail.com>
2021-11-30 13:11:32 +0300

    Add files via upload

---
+++ lab01.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+
+<?php
+
+<form method="post" action =lab01.php>
+
+please enter the radius of the circle:
+
+<input type="text" name="radius">
+<input type="sumbit">
+</form>
+?>
+</body>
+</html>
